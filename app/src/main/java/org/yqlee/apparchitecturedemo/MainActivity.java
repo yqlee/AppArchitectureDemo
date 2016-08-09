d8f54534c044839598f70e2515e98d5e8d565a17
yqlee <yqlee890@gmail.com>
2016-08-09 16:36:53 +0800

    Initial commit

---
+++ app/src/main/java/org/yqlee/apparchitecturedemo/MainActivity.java
@@ -0,0 +1,13 @@
+package org.yqlee.apparchitecturedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
